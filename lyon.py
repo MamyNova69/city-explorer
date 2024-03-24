61dbf2800d4a0104f6d563807b01cc366a64338e
Rémi Curie <34945270+MamyNova69@users.noreply.github.com>
2024-03-24 21:34:16 +0100

    Add files via upload

---
+++ lyon.py
@@ -0,0 +1,124 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import navigateur
+import datetime
+import time
+
+e = datetime.datetime.now()
+timeforcsv = e.strftime("%d-%m-%Y %HH%M")
+
+def ecrire_dans_csv_ligne(nouvelle_ligne) : # liste ou string
+	with open(file_name, mode='a', newline='', encoding='utf-8') as fichier:
+		writer = csv.writer(fichier, delimiter=';')
+    	# Écrire la nouvelle ligne dans le fichier CSV
+		writer.writerow(nouvelle_ligne)
+
+def find_accept_cookie():
+	button = navigateur.driver.find_elements(By.XPATH, "//button[@type='button' and text()='OK']")
+	button[0].click()
+
+def next_page():
+	global page_suivante
+	page_suivante = navigateur.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[@aria-label='Suivant']")))
+	# print(next_page[0].get_attribute("href"))
+	if len(page_suivante) !=0 :
+		print("Page suivante")
+		return True
+	else :
+		print("pas de page suivante")
+		return False
+
+def find_airbnb():
+	# navigateur.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-testid='card-container']")))
+	airbnb = navigateur.driver.find_elements(By.XPATH, "//div[@data-testid='card-container']")
+	for i in airbnb :
+		ligne = []
+		# print(i.text)
+		link = i.find_element(By.XPATH, ".//a[@href]")
+		# print(link.get_attribute("href"))
+		base_url = extract_base_url(link.get_attribute("href"))
+		print(base_url)
+		ligne.append(base_url)
+		airbnb_Lyon.append(link.get_attribute("href"))
+		ecrire_dans_csv_ligne(ligne)
+
+
+def extract_base_url(url):
+    return url.split('?')[0]
+
+
+# utilisation de filtre sur le prix pour réduire le nombre de résultat :
+# https://www.airbnb.fr/s/Lyon/homes?&price_min=150&price_max=160
+min = (range(0, 250, 10))
+max = (range(11, 251, 10))
+
+url_to_scrap = ("https://www.airbnb.fr/s/Lyon/homes?&price_min="+str(min)+"&price_max="+str(max))
+
+# creer une liste pour chaque collones du fichier csv :
+file_name = f"{timeforcsv}_airbnb.csv"
+titres = ["URL"]
+ecrire_dans_csv_ligne(titres)
+airbnb_Lyon = []
+
+
+URLS_TO_SCRAPP = []
+min_values = range(15, 99, 1)
+max_values = range(16, 100, 1)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+min_values = range(100, 248, 2)
+max_values = range(102, 250, 2)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+# pour les dernières pages augmentation des ranges
+min_values = range(250, 495, 5)
+max_values = range(255, 500, 5)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+# pour les dernières pages augmentation des ranges
+min_values = range(500, 990, 10)
+max_values = range(510, 1000, 10)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+# pour les dernières pages augmentation des ranges
+min_values = range(1000, 1950, 50)
+max_values = range(1050, 2000, 50)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+# pour les dernières pages augmentation des ranges
+min_values = range(2000, 4900, 100)
+max_values = range(2100, 5000, 100)
+for min_val, max_val in zip(min_values, max_values):
+    url_to_scrap = "https://www.airbnb.fr/s/Lyon/homes?&price_min=" + str(min_val) + "&price_max=" + str(max_val) + "&pagination_search=true&l2_property_type_ids[]=1&l2_property_type_ids[]=3"
+    URLS_TO_SCRAPP.append(url_to_scrap)
+
+url_to_scrap = ("https://www.airbnb.fr/s/Lyon/homes")
+
+
+
+if __name__ == "__main__":
+
+	navigateur.ouvrir_session_chrome()
+	navigateur.driver.get(url_to_scrap)
+	time.sleep(5)
+	find_accept_cookie()
+	time.sleep(5)
+
+	for x in URLS_TO_SCRAPP:
+		navigateur.driver.get(x)
+		print(f"je scrapp l'URL suivante : {x}")
+		time.sleep(4)
+		find_airbnb()
+		while next_page() != False :
+			page_suivante[0].click()
+			time.sleep(3)
+			find_airbnb()
+
+
+	navigateur.fermer_session_chrome()
---
+++ navigateur.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+def ouvrir_session_chrome():
+	chrome_options = webdriver.ChromeOptions()
+	# service = webdriver.ChromeService(executable_path="chromedriver.exe")
+	# A telecharger ici : https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json
+	# chrome_options.add_argument('--ignore-certificate-errors')
+	# chrome_options.add_argument('--allow-insecure-localhost')
+	# chrome_options.add_argument('--disable-web-security')
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--mute-audio")
+	chrome_options.add_argument('--log-level=3')
+	# chrome_options.add_argument("--remote-debugging-port=12345") # select a port
+	# chrome_options.add_argument('--incognito')
+	# chrome_options.add_argument("start-maximized")
+	chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+	chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+	chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+	chrome_options.add_experimental_option("useAutomationExtension", False)
+	# path_to_extension = r"ublock/uBlock-Origin.crx"
+	# chrome_options.add_extension(path_to_extension)
+	global driver
+	driver = webdriver.Chrome(options=chrome_options) # add service=service if local path is used
+	driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
+	global wait
+	wait = WebDriverWait(driver, 20)
+
+
+def fermer_session_chrome():
+	driver.quit()
+
+def refresh():
+	driver.refresh()
+
+def open_newtab(url):
+    driver.switch_to.new_window('tab')
+    driver.get(url)
